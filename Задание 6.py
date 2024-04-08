1b5d38ff5690d5fe3f86ccbbbf267fd50dd980d6
AlferovDmitry <157224027+AlferovDmitry@users.noreply.github.com>
2024-04-08 21:15:57 +0300

    Add files via upload

---
+++ Задание 6.py
@@ -0,0 +1,8 @@
+#6 Создайте переменную для вашего любимого числа. Выведите сообщение вида “Моё любимое число: {число}”.
+
+#Решение
+favorite_number = 7
+print("My favorite number:", favorite_number)
+
+#Итог
+#My favorite number: 7
---
+++ Задание 8.py
@@ -0,0 +1,10 @@
+#8 Введите любое слово и выведите его, повторив три раза.
+
+#Решение
+word = input("Enter a word: ")
+print(word * 3)
+
+#Итог
+#Enter a word:
+#cat
+#catcatcat
